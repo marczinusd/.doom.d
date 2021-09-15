c048d77b30fc5974e68549aee97561bf35cd263a
David Marczinus <marczinus.david@gmail.com>
2021-09-15 22:04:22 +0200

    initial commit

---
+++ custom.el
@@ -0,0 +1,51 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
+ '(exwm-floating-border-color "#d6d4d4")
+ '(fci-rule-color "#a3a1a1")
+ '(highlight-tail-colors
+   ((("#f0f3e5" "#669900" "green")
+     . 0)
+    (("#f3f8f7" "#8abeb7" "cyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#f2f2f2" "#4271ae"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f2f2f2" "#718c00"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f2f2f2" "#8e908c"))
+ '(objed-cursor-color "#c82829")
+ '(pdf-view-midnight-colors (cons "#4d4d4c" "#ffffff"))
+ '(rustic-ansi-faces
+   ["#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#c678dd" "#8abeb7" "#4d4d4c"])
+ '(vc-annotate-background "#ffffff")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#718c00")
+    (cons 40 "#999a00")
+    (cons 60 "#c1a800")
+    (cons 80 "#eab700")
+    (cons 100 "#eda70a")
+    (cons 120 "#f19714")
+    (cons 140 "#f5871f")
+    (cons 160 "#e5825e")
+    (cons 180 "#d57d9d")
+    (cons 200 "#c678dd")
+    (cons 220 "#c65da1")
+    (cons 240 "#c74265")
+    (cons 260 "#c82829")
+    (cons 280 "#b94141")
+    (cons 300 "#ab5c5a")
+    (cons 320 "#9c7673")
+    (cons 340 "#a3a1a1")
+    (cons 360 "#a3a1a1")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
